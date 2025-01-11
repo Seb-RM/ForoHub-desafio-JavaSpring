3574401f2866055d053edefe45025fb1b6f87176
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-01-10 22:50:31 -0300

    Initial commit

---
+++ src/main/java/com/aluracursos/ForoHub_desafio_JavaSpring/ForoHubDesafioJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.ForoHub_desafio_JavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubDesafioJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubDesafioJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/ForoHub_desafio_JavaSpring/ForoHubDesafioJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.ForoHub_desafio_JavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubDesafioJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
