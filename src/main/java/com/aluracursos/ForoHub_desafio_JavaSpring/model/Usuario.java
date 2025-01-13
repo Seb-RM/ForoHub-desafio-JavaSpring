7efcd26723191e6f3cc4ed0c9b75ca1b15bb0041
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-01-13 12:57:42 -0300

    se agrega entidad Usuario

---
+++ src/main/java/com/aluracursos/ForoHub_desafio_JavaSpring/model/Usuario.java
@@ -0,0 +1,36 @@
+package com.aluracursos.ForoHub_desafio_JavaSpring.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String correoElectronico;
+
+    @Column(nullable = false)
+    private String contrasena;
+
+    @ManyToMany
+    @JoinTable(
+            name = "usuario_perfil",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "perfil_id")
+    )
+    private List<Perfil> perfiles = new ArrayList<>();
+}
