f9d41b1ac32e09012c3a809f4376ee04c9ce661c
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2025-01-13 12:56:38 -0300

    se agrega entidad Topico

---
+++ src/main/java/com/aluracursos/ForoHub_desafio_JavaSpring/model/Topico.java
@@ -0,0 +1,45 @@
+package com.aluracursos.ForoHub_desafio_JavaSpring.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String mensaje;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusTopico status;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id", nullable = false)
+    private Usuario autor;
+
+    @ManyToOne
+    @JoinColumn(name = "curso_id", nullable = false)
+    private Curso curso;
+
+    @OneToMany(mappedBy = "topico", cascade = CascadeType.ALL)
+    private List<Respuesta> respuestas = new ArrayList<>();
+}
+
